d6474f72bbbba28789674a5b5fe0d13b9b4360e3
mhmoudmhmed <mhmoudmhmed21@gmail.com>
2020-05-07 05:22:45 +0200

    Weather App by React.js updated

--- src/From.js
+++ src/From.js
@@ -46,12 +46,12 @@ class Form extends PureComponent  {
                 })
             } else {
                 this.setState({
-                    city : '', //data.name
-                    main: '', //data.weather[0].main
-                    country: '', //data.sys.country
-                    lat: '', // data.coord.lat
-                    lon: '', // data.coord.lon
-                    desc: '', // data.weather[0].description,
+                    city : '',
+                    main: '',
+                    country: '',
+                    lat: '',
+                    lon: '',
+                    desc: '',
                     error: "Enter Correct Name"
                 })
             }
